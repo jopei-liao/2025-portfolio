a5e4e0a789a88d391182d70c70955b9123ee9c65
廖珮宇 <abbypyl@gmail.com>
2026-03-05 21:52:42 +0000

    feat: revamp Home page layout and styles
    - Replaced repetitive headings with a structured intro box for better presentation.
    - Updated styles for the Home page to improve responsiveness and visual hierarchy.
    - Adjusted height calculation for the page to enhance layout consistency across devices.

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,13 +5,38 @@ const Home = () => {
 		<div className="home page flex justify-center align-center">
 			<div className="container">
 				<div className="scroll-box">
-					<h1>Jopei Liao</h1>
-					<h1>Jopei Liao</h1>
-					<h1>Jopei Liao</h1>
-					<h1>Jopei Liao</h1>
-					<h1>Jopei Liao</h1>
-					<h1>Jopei Liao</h1>
-					<h2>Frontend Engineer</h2>
+					<div className="intro-box flex flex-col justify-center align-center">
+						<h1>
+							Hi,
+							<br /> I'm Jopei
+						</h1>
+						<h2>A curious Front-end Engineer with 5 years of experience</h2>
+						<h3>Nice to meet you!</h3>
+					</div>
+					<div className="intro-box flex flex-col justify-center align-center">
+						<h1>
+							Hi,
+							<br /> I'm Jopei
+						</h1>
+						<h2>A curious Front-end Engineer with 5 years of experience</h2>
+						<h3>Nice to meet you!</h3>
+					</div>
+					<div className="intro-box flex flex-col justify-center align-center">
+						<h1>
+							Hi,
+							<br /> I'm Jopei
+						</h1>
+						<h2>A curious Front-end Engineer with 5 years of experience</h2>
+						<h3>Nice to meet you!</h3>
+					</div>
+					<div className="intro-box flex flex-col justify-center align-center">
+						<h1>
+							Hi,
+							<br /> I'm Jopei
+						</h1>
+						<h2>A curious Front-end Engineer with 5 years of experience</h2>
+						<h3>Nice to meet you!</h3>
+					</div>
 				</div>
 			</div>
 		</div>
