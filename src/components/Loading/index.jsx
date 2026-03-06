ee17e5c73e41ffc11afc4b73b1052f9a9031163a
廖珮宇 <abbypyl@gmail.com>
2026-03-06 14:11:30 +0000

    feat: update ESLint configuration and enhance styles across components
    - Refactored ESLint configuration for improved readability and consistency.
    - Added Tailwind CSS utilities and custom scrollbar styles in CSS.
    - Updated layout styles in SASS to remove scrollbar visibility and improve responsiveness.
    - Adjusted loading component styles for better alignment and visual appeal.
    - Introduced new theme color variables for better design consistency.

--- src/components/Loading/index.jsx
+++ src/components/Loading/index.jsx
@@ -3,16 +3,16 @@ import "./style.sass";
 
 const Loading = forwardRef((props, ref) => {
 	return (
-		<div className="loading fixed h-dvh w-full z-9999" ref={ref}>
-			<div className="container h-full w-full">
-				<div className="loading-text flex items-center justify-center">
-					<span className="loading-text-words">L</span>
-					<span className="loading-text-words">O</span>
-					<span className="loading-text-words">A</span>
-					<span className="loading-text-words">D</span>
-					<span className="loading-text-words">I</span>
-					<span className="loading-text-words">N</span>
-					<span className="loading-text-words">G</span>
+		<div className="loading fixed w-full h-screen-vh z-9999" ref={ref}>
+			<div className="container w-full h-full">
+				<div className="loading-text flex items-center justify-center text-[1.5em] font-bold font-[Roboto_Mono]">
+					<span className="m-[0_.25em]">L</span>
+					<span className="m-[0_.25em]">O</span>
+					<span className="m-[0_.25em]">A</span>
+					<span className="m-[0_.25em]">D</span>
+					<span className="m-[0_.25em]">I</span>
+					<span className="m-[0_.25em]">N</span>
+					<span className="m-[0_.25em]">G</span>
 				</div>
 			</div>
 		</div>
