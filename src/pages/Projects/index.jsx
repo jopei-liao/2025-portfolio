da2e50d2cc7d2ab3cd8fc23dd7b98dea164c7617
廖珮宇 <abbypyl@gmail.com>
2026-03-10 22:29:14 +0000

    feat: update Projects page with new project data structure and enhanced styling
    - Added new project entries with detailed information including titles, subtitles, banners, and links.
    - Introduced new theme colors in CSS for improved visual consistency.
    - Enhanced layout and interactivity of project items, including a banner image and improved tag display.
    - Integrated new icons for navigation and close actions in the project lightbox.
    - Updated SASS styles for better visual hierarchy and responsiveness.

--- src/pages/Projects/index.jsx
+++ src/pages/Projects/index.jsx
@@ -4,6 +4,9 @@ import axios from "axios";
 
 import "./style.sass";
 
+import ic_arrow from "@/assets/images/ic-arrow.png";
+import ic_close from "@/assets/images/ic-close.png";
+
 const Projects = () => {
 	const [projectsData, setProjectsData] = useState([]);
 	const { id } = useParams(); // 抓取網址上的 id
@@ -29,42 +32,127 @@ const Projects = () => {
 	return (
 		<div className="projects page page-shell">
 			<div className="page-container pt-13">
-				<h1 className="text-2xl md:text-4xl px-7 pb-4 md:px-10 font-bold">Projects</h1>
-				<div className="scroll-box h-[90%] px-6 pb-14 md:px-10 md:pb-18 box-border overflow-y-scroll scrollbar-hidden">
-					<div className="projects-list grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-2">
+				<h1 className="relative text-2xl md:text-4xl text-theme-red font-bold px-7 pb-2 md:px-10">Projects</h1>
+				<div className="scroll-box h-[90%] px-6 pb-14 pt-10 md:px-10 md:pb-18 box-border overflow-y-scroll scrollbar-hidden">
+					<div className="projects-list grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-10">
 						{/* project list */}
 						{projectsData.map(project => (
-							<div key={project.id} className="item w-full p-4 md:p-5 mb-6 box-border rounded-sm shadow-xl" onClick={() => navigate(`/projects/${project.id}`)}>
-								<h2 className="text-base md:text-xl mb-2">{project.title}</h2>
-								{project.tag && (
-									<div className="flex flex-wrap gap-2">
-										{project.tag.map(tag => (
-											<span key={tag} className="text-sm md:text-base">
+							<div key={project.id} className="item relative w-full min-h-[300px] box-border rounded-sm shadow-xl" onClick={() => navigate(`/projects/${project.id}`)}>
+								<div className="banner mb-4">
+									<img src={project.banner} />
+								</div>
+								<div className="txt-box p-5">
+									<h2 className="text-base text-theme-red font-bold md:text-xl mb-3">{project.title}</h2>
+									<p className="text-sm text-font-black mb-20">{project.subtitle}</p>
+									{project.tag && (
+										<div className="absolute bottom-5 left-5 flex flex-wrap gap-2 ">
+											{project.tag.map(tag => (
+												<span key={tag} className="text-sm md:text-base text-font-black border py-[.15em] px-3 rounded-full">
+													{tag}
+												</span>
+											))}
+										</div>
+									)}
+								</div>
+								<button className="absolute right-4 md:bottom-5 bottom-4 size-8.5 md:hover:translate-x-1 md:hover:duration-150 duration-150 cursor-pointer">
+									<div className="img">
+										<img src={ic_arrow} alt="" />
+									</div>
+								</button>
+							</div>
+						))}
+					</div>
+				</div>
+				{/* Lightbox：if URL has id */}
+				{selectedProject && (
+					<div className="project-box fixed top-0 left-0 w-full h-full bg-black/50 flex items-center justify-center z-999">
+						<div className="relative lightbox-container w-[89%] xl:w-full h-[90%] pt-13 pb-12 px-6 md:pt-13 md:px-13">
+							<div className="title-box flex justify-between absolute top-0 left-0 w-full pl-6 pr-5 md:px-12 pt-10">
+								<h2 className="text-2xl md:text-3xl text-theme-red font-bold">{selectedProject.title}</h2>
+								<button className="size-10 md:hover:scale-105 md:hover:duration-100 duration-100 cursor-pointer -translate-y-1" onClick={() => navigate("/projects")}>
+									<div className="img">
+										<img src={ic_close} alt="" />
+									</div>
+								</button>
+							</div>
+							<div className="scroll-box h-[90%] mt-10 pt-10 box-border overflow-y-scroll scrollbar-hidden">
+								<p className="text-sm text-font-black text-justify mb-8">{selectedProject.overview}</p>
+								{selectedProject.tag && (
+									<div className="tech-box flex flex-wrap gap-2 mb-4">
+										{selectedProject.tag.map(tag => (
+											<span key={tag} className="text-sm md:text-base text-white bg-theme-blue py-1 px-3.5 rounded-full">
 												{tag}
 											</span>
 										))}
 									</div>
 								)}
-							</div>
-						))}
-					</div>
-
-					{/* Lightbox：if URL has id */}
-					{selectedProject && (
-						<div className="project-box fixed top-0 left-0 w-full h-full bg-black/50 flex items-center justify-center z-99999">
-							<div className="relative lightbox-container w-[89%] xl:w-full h-[90%] pt-13 pb-12 px-6 md:pt-13 md:px-13">
-								<button
-									className="size-10 absolute top-3 right-3 md:top-6 md:right-6 block text-4xl text-center"
-									onClick={() => navigate("/projects")} // when close, go back to /projects
-								>
-									×
-								</button>
-								<h2 className="text-xl md:text-2xl font-bold mb-6">{selectedProject.title}</h2>
-								<p className="text-sm">{selectedProject.description}</p>
+								<ul className="link-box text-sm mb-8 text-theme-blue">
+									<span className="mr-1">🔗</span>
+									{selectedProject.links && (
+										<>
+											{selectedProject.links.demo && (
+												<li className="inline mr-4">
+													<a href={selectedProject.links.demo} target="_blank" rel="noopener noreferrer" className="underline">
+														Demo
+													</a>
+												</li>
+											)}
+											{selectedProject.links.github && selectedProject.links.github !== "" && (
+												<li className="inline">
+													<a href={selectedProject.links.github} target="_blank" rel="noopener noreferrer" className="underline">
+														GitHub
+													</a>
+												</li>
+											)}
+										</>
+									)}
+								</ul>
+								{selectedProject.challenge && (
+									<div className="challenge-box text-font-black mb-6">
+										<h3 className="text-xl font-bold">Challenge</h3>
+										<ul className="text-sm leading-normal divide-y divide-font-black">
+											{Array.isArray(selectedProject.challenge) ? (
+												selectedProject.challenge.map((item, idx) => {
+													const key = Object.keys(item)[0];
+													const value = item[key];
+													return (
+														<li key={idx} className="flex py-8">
+															<p className="w-[35%] shrink-0 font-bold">{key}</p>
+															<p>{value}</p>
+														</li>
+													);
+												})
+											) : (
+												<li>{selectedProject.challenge}</li>
+											)}
+										</ul>
+									</div>
+								)}
+								{selectedProject.impact && (
+									<div className="impact-box text-font-blue mb-4">
+										<h3 className="text-2xl font-bold">Impact</h3>
+										<ul className="text-sm leading-normal divide-y divide-font-blue">
+											{Array.isArray(selectedProject.impact) ? (
+												selectedProject.impact.map((item, idx) => {
+													const key = Object.keys(item)[0];
+													const value = item[key];
+													return (
+														<li key={idx} className="flex py-8">
+															<p className="w-[35%] shrink-0 font-bold">{key}</p>
+															<p>{value}</p>
+														</li>
+													);
+												})
+											) : (
+												<li>{selectedProject.impact}</li>
+											)}
+										</ul>
+									</div>
+								)}
 							</div>
 						</div>
-					)}
-				</div>
+					</div>
+				)}
 			</div>
 		</div>
 	);
